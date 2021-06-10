b328e5547c26fac4f7bc71169b9289f18475e3ed
Ashkan Jalali <55811147+ashkanjalaliQ@users.noreply.github.com>
2021-06-11 00:15:06 +0430

    add length

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from ta.trend import cci as CCI
 from ta.momentum import StochasticOscillator as SR
 from ta.trend import EMAIndicator
 from ta.trend import SMAIndicator
+import matplotlib.pyplot as plt
 
 
 class History:
@@ -32,8 +33,12 @@ class History:
         return pd.DataFrame(prices_data)
 
     def __try_requests(self, url):
-        while (result := requests.get(url, params=self.params)).status_code != 200:
-            pass
+        try:
+            while (result := requests.get(url, params=self.params)).status_code != 200:
+                pass
+        except ConnectionError:
+            raise print('ALARM: Your Net is lost. Please Check the Connection!')
+
         return result
 
     def __requests_to(self, url, is_json=True):
@@ -77,23 +82,33 @@ class Indicators:
     def macd(self, key='close', slow=26, fast=12, sign=9):
         return MACD(self.price_data[key], window_slow=slow, window_fast=fast, window_sign=sign)
 
-    def cci(self):
+    def cci(self, length=14):
         return CCI(
             self.price_data['high'],
             self.price_data['low'],
-            self.price_data['close']
+            self.price_data['close'],
+            window=length
         )
 
     def rsi(self, key='close', length=14):
         return RSI(self.price_data[key], window=length)
 
-    def sr(self):
+    def sr(self, length=21, length_smooth=8):
         return SR(
             self.price_data['high'],
             self.price_data['low'],
-            self.price_data['close']
+            self.price_data['close'],
+            window=length,
+            smooth_window=length_smooth
         )
-
+        '''data = {}
+        data['14-high'] = self.price_data['high'].rolling(21).max()
+        data['14-low'] = self.price_data['low'].rolling(21).min()
+        data['stoch_stoch'] = (self.price_data['close'] - data['14-low']) * 100 / (data['14-high'] - data['14-low'])
+        data['stoch_signal'] = data['stoch_stoch'].rolling(8).mean()
+        print(data['stoch_signal'].values[-1], data['stoch_stoch'].values[-1])
+        return data
+'''
     def ema(self, key='close', length=14):
         return EMAIndicator(self.price_data[key], window=length)
 
@@ -118,8 +133,11 @@ class Analyzer:
             'mfi': list(indicators.mfi()),
             'rsi': list(indicators.rsi()),
             'cci': list(indicators.cci()),
+            #'stoch_stoch': list(indicators.sr()['stoch_stoch'])[-1],
+            #'stoch_signal': list(indicators.sr()['stoch_signal'])[-1],
             'stoch_stoch': list(indicators.sr().stoch()),
             'stoch_signal': list(indicators.sr().stoch_signal()),
+            'ema7': list(indicators.ema(length=7).ema_indicator()),
             'ema20': list(indicators.ema(length=20).ema_indicator()),
             'ema25': list(indicators.ema(length=25).ema_indicator()),
             'ema30': list(indicators.ema(length=30).ema_indicator()),
@@ -127,7 +145,9 @@ class Analyzer:
             'ema40': list(indicators.ema(length=40).ema_indicator()),
             'ema45': list(indicators.ema(length=45).ema_indicator()),
             'ema50': list(indicators.ema(length=50).ema_indicator()),
-            'ema55': list(indicators.ema(length=55).ema_indicator())
+            'ema55': list(indicators.ema(length=55).ema_indicator()),
+            'ema233': list(indicators.ema(length=233).ema_indicator()),
+            'sma100': list(indicators.sma(length=100).sma_indicator())
         }
         self.codes = codes
         self.results = []
@@ -152,15 +172,27 @@ class OrderList:
         self.order = []
         self.coins = []
 
+    def _cleaner(self, ls):
+        ls_cleaned = []
+        for i in range(len(ls)):
+            if ls[i] != '':
+                ls_cleaned.append(ls[i])
+        return ls_cleaned
+
     def get_user_orders(self):
-        with open('main.we', 'r') as reader:
+        with open('main.txt', 'r') as reader:
             orders = reader.read().split('\n')
-        return orders
+        return self._cleaner(orders)
 
     def get_user_coins(self):
-        with open('markets.we', 'r') as reader:
+        with open('markets.txt', 'r') as reader:
             coins = reader.read().split('\n')
-        return coins
+        return self._cleaner(coins)
+
+    def get_user_timeframes(self):
+        with open('timeframes.txt') as reader:
+            timeframes = reader.read().split('\n')
+        return self._cleaner(timeframes)
 
     def __str__(self):
         return self.order
@@ -168,10 +200,7 @@ class OrderList:
 orderlist = OrderList()
 orders = orderlist.get_user_orders()
 coins = orderlist.get_user_coins()
-
-timeframes = [
-    '1day'
-]
+timeframes = orderlist.get_user_timeframes()
 
 history = History()
 
@@ -183,4 +212,6 @@ for coin in coins:
 
         analyzer = Analyzer(indicators, orders)
         if analyzer.interpret_code():
-            print(f'{coin}: timeframe {timeframe}')
+            print(f'{coin}: timeframe {timeframe} => Found')
+        #else:
+            #print(f'{coin}: timeframe: {timeframe} => Checked.')
